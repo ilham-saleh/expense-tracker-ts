f9b8a50c0e5162b8ecf636b3015599e3a418632c
ilham saleh <ilham2saleh@gmail.com>
2024-04-15 19:46:34 +0100

    Add initial data

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const expenses = [
+    {
+        description: "Flower",
+        amount: 3.50,
+        category: "Grocery"
+    },
+    {
+        description: "Movie",
+        amount: 7.49,
+        category: "Entertainment"
+    },
+    {
+        description: "Gas",
+        amount: 170,
+        category: "Utilities"
+    },
+
+]
