9bfa08469ef5105ac4a75575b57d2e7f6c533a1b
ilham saleh <ilham2saleh@gmail.com>
2024-04-16 16:48:05 +0100

    Implement filter functionality

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,25 @@ import data from "./data.ts";
 import ExpenseFilter from "./components/ExpenseFilter.tsx";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState(data);
 
   const handleDelete = (id: number) => {
     setExpenses(expenses.filter((expense) => expense.id !== id));
   };
 
+  const filteredList = expenses.filter((expense) =>
+    selectedCategory ? expense.category === selectedCategory : expenses
+  );
   return (
     <div>
-      <ExpenseFilter />
-      <ExpenseList expenses={expenses} onDelete={(id) => handleDelete(id)} />
+      <div className="mb-3">
+        <ExpenseFilter onSelect={(category) => setSelectedCategory(category)} />
+        <ExpenseList
+          expenses={filteredList}
+          onDelete={(id) => handleDelete(id)}
+        />
+      </div>
     </div>
   );
 }
--- src/components/ExpenseFilter.tsx
+++ src/components/ExpenseFilter.tsx
@@ -1,11 +1,18 @@
-const ExpenseFilter = () => {
+interface Props {
+  onSelect: (category: string) => void;
+}
+
+const ExpenseFilter = ({ onSelect }: Props) => {
   return (
     <div>
-      <select className="form-select">
+      <select
+        className="form-select"
+        onChange={(event) => onSelect(event.target.value)}
+      >
         <option value="">All categories</option>
-        <option value="">All categories</option>
-        <option value="">All categories</option>
-        <option value="">All categories</option>
+        <option value="Utilities">Utilities</option>
+        <option value="Entertainment">Entertainment</option>
+        <option value="Grocery">Grocery</option>
       </select>
     </div>
   );
