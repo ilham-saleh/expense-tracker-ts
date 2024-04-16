cbd6e6593f0b282a683f980ab02bb84de7b46670
ilham saleh <ilham2saleh@gmail.com>
2024-04-16 18:17:28 +0100

    Create handleChange to get input data and save in state

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,6 +1,35 @@
+import { FormEvent, useState } from "react";
 import { categories } from "../App";
 
+// interface FormData {
+//   description: string;
+//   amount: number;
+//   category: string;
+// }
+
+// interface Props {
+//   onFormSubmit: (FormData: {}) => void;
+// }
+
 const ExpenseForm = () => {
+  const [formData, setFormData] = useState({
+    description: "",
+    amount: 0,
+    category: "",
+  });
+
+  const handleChange = (e: FormEvent) => {
+    const { name, value } = e.target as HTMLInputElement;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault(); // Prevent default form submission behavior
+    console.log(formData); // Pass form data to the parent component
+  };
+
+  console.log(formData);
   return (
     <div className="mb-3">
       <form>
@@ -8,21 +37,41 @@ const ExpenseForm = () => {
           <label htmlFor="description" className="form-label">
             Description
           </label>
-          <input id="description" type="text" className="form-control" />
+          <input
+            onChange={(e) => handleChange(e)}
+            id="description"
+            name="description"
+            type="text"
+            className="form-control"
+            value={formData.description}
+          />
         </div>
 
         <div className="mb-3">
           <label htmlFor="amount" className="form-label">
             Amount
           </label>
-          <input id="amount" type="number" className="form-control" />
+          <input
+            onChange={(e) => handleChange(e)}
+            id="amount"
+            type="number"
+            name="amount"
+            className="form-control"
+            value={formData.amount}
+          />
         </div>
 
         <div className="mb-3">
           <label htmlFor="category" className="form-label">
             Category
           </label>
-          <select name="" id="category" className="form-select">
+          <select
+            onChange={(e) => handleChange(e)}
+            name="category"
+            id="category"
+            className="form-select"
+            value={formData.category}
+          >
             <option value=""></option>
             {categories.map((category) => (
               <option key={category} value={category}>
